454c97d50ae6a2b50008d85f50e55cf8a3b6d3e1
emmafewer <emmafewer20@gmail.com>
2020-12-09 15:04:12 -0800

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+        "My name is Emma"
+    end 
+
+    get '/hometown' do
+        "My hometown is Glasgow"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is I can't decide"
+    end 
 end
